3d622405eb9b2fa76b6b7dc5154aff05a7aa4f75
garhajas <garhajas@my.yorku.ca>
2020-01-05 00:26:06 -0500

    Update iimaper.py

--- part2/iimaper.py
+++ part2/iimaper.py
@@ -13,25 +13,3 @@ for row in input_file:
 	cat = row[12].split(';')
 	for elem in cat:
 		print(elem + '\t' +  bid)
-	'''
-	table = []
-	table.append(' : '.join([row[0], row[12]]))
-	#table = '\n'.join(table)
-
-	table = re.split(',', str(table))
-	#table = [re.sub('^a-zA-Z0-9_-:;+', '', item) for item in table]
-	print(table)
-	
-	for elem in table:
-		x = elem.split(' : ')[0]
-		x = re.sub("'", '', x)
-		x = re.sub('"', '', x)
-		y = elem.split(' : ')[1]
-		y = re.sub('"', '', y)
-		y = re.sub("'", '', y)
-		z = y.split(';')
-		for i in z:
-			print(i.strip() + '\t' + x.strip())
-		#print(z)
-		#print(elem.split(';'))
-	'''
